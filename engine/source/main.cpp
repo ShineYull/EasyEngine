7191cac4a2a6ac224d6255c050c02a3d2286b361
ShineYu <shine.yull@foxmail.com>
2022-10-28 01:47:56 +0800

    Pass in a vertex color value to interpolate the color of the triangle

--- engine/source/main.cpp
+++ engine/source/main.cpp
@@ -11,24 +11,20 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
-"out vec4 vertexColor;\n"
+"layout (location = 1) in vec3 myColor;\n"
+"out vec3 vertexColor;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos, 1.0);\n"
-"   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);"
+"   vertexColor = myColor;"
 "}\0";
 
 const char* fragmentShader1Source = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"in vec4 vertexColor;\n"
-"uniform vec4 myColorThere;\n"
+"in vec3 vertexColor;\n"
 "void main()\n"
 "{\n"
-"   vec4 myColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"   vec3 myColorOne = vec3(1.0f, 0.5f, 0.2f);\n"
-"   vec2 myColorTwo = vec2(1.0f, 0.5f);\n"
-"   FragColor = vec4(myColorTwo.r, myColorTwo.g, myColorOne.b, myColor.a);\n"
-"   FragColor = myColorThere;"
+"   FragColor = vec4(vertexColor, 1.0f);"
 "}\n\0";
 
 int main() {
@@ -59,9 +55,10 @@ int main() {
     }
 
     float firstTriangle[] = {
-        -0.5f, -0.5f, 0.0f,  // left 
-        0.5f, -0.5f, 0.0f,  // right
-        0.0f, 0.5f, 0.0f,  // top 
+        // 位置              // 颜色
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
     };
     unsigned int VAOs, VBOs;
     glGenVertexArrays(1, &VAOs);
@@ -70,8 +67,14 @@ int main() {
     glBindVertexArray(VAOs);
     glBindBuffer(GL_ARRAY_BUFFER, VBOs);
     glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+    /* position attrib */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    /* color attrib */
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     unsigned int fragmentShaderOrange = glCreateShader(GL_FRAGMENT_SHADER);
@@ -89,8 +92,6 @@ int main() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShaderOrange);
 
-    int vertexColorLocation = glGetUniformLocation(shaderProgramOrange, "myColorThere");
-
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
@@ -99,13 +100,8 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgramOrange);
-
-        float timeValue = glfwGetTime();
-        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-
         glBindVertexArray(VAOs);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
