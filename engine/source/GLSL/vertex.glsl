7ec3abdeba52caf3d01d0d70f744bd2af0a8084d
ShineYu <shine.yull@foxmail.com>
2022-10-28 20:37:12 +0800

    The lower left corner of the triangle is black because the lower left corner belongs to the negative area, so when the color value is negative, it is black.

--- engine/source/GLSL/vertex.glsl
+++ engine/source/GLSL/vertex.glsl
@@ -2,13 +2,13 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
-out vec3 ourColor;
+out vec4 ourColor;
 
 uniform vec4 offerset;
 
 void main()
 {
-    //gl_Position = vec4(aPos.x, -aPos.y, aPos.z, 1.0f);
-    gl_Position = vec4(aPos.x, -aPos.y, aPos.z, 1.0f) + offerset;
-    ourColor = aColor;
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);
+    //gl_Position = vec4(aPos.x, -aPos.y, aPos.z, 1.0f) + offerset;
+    ourColor = vec4(aPos.x, aPos.y, aPos.z, 1.0f);
 }
