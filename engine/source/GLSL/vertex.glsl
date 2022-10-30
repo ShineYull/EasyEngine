febc8ca52c98b888676e1498a7942b43b7f79bb2
ShineYu <shine.yulingle@gmail.com>
2022-10-31 00:34:51 +0800

    Add comment to vertex shader script

--- engine/source/GLSL/vertex.glsl
+++ engine/source/GLSL/vertex.glsl
@@ -8,6 +8,13 @@ out vec2 TexCoord;
 
 uniform float factor1;
 
+/**
+  * |  cos0  -sin0   0   0|    |x|    |x * cos0 - y * sin0|
+  * |  sin0   cos0   0   0|  * |y| =  |x * sin0 + y * cos0|
+  * |  0       0     1   0|    |z|    |         z         |
+  * |  0       0     0   1|    |w|    |         1         |
+  * You can rotate a coordinate system using a matrix.
+  */
 mat4 rotate3d(float _angle) {
 	return mat4(
 		cos(_angle), -sin(_angle), 0.0f, 0.0f,
